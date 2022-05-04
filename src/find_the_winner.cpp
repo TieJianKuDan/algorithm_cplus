267ccafdfd5b6ae45a577c202b2e3374d0e528be
tiejiankudan <893507220@qq.com>
2022-05-04 10:30:00 +0800

    1823. 找出游戏的获胜者（改进）

--- src/find_the_winner.cpp
+++ src/find_the_winner.cpp
@@ -2,7 +2,7 @@
  * @Author: TieJianKuDan
  * @Date: 2022-05-04 09:33:04
  * @LastEditors: TieJianKuDan
- * @LastEditTime: 2022-05-04 10:03:09
+ * @LastEditTime: 2022-05-04 10:29:43
  * @FilePath: \AlgorithmProblem\src\find_the_winner.cpp
  * @Description: 1823. 找出游戏的获胜者
  * Copyright (c) 2022 by TieJianKuDan, All Rights Reserved. 
@@ -14,6 +14,7 @@ using namespace std;
 
 class Solution {
 public:
+    // 数组模拟
     int findTheWinner(int n, int k) {
         vector<int> friends;
         int start = 0;
@@ -30,6 +31,17 @@ public:
             start = end % (n - i);
         }
         return friends[0];
+    }
+    // 数学推导
+    int findTheWinnerPro(int n, int k) {
+        return help(n, k) + 1;
+    }
+    int help(int n, int k) {
+        if (n == 1) {
+            return 0;
+        } else {
+            return (help(n - 1, k) + k) % (n - 1);
+        }
     }
 };
 
